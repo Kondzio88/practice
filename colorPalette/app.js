616ccd1acd4752565b77e794bad13dbb8169b818
gumol <jacoszekjacoszek@o2.pl>
2026-05-05 11:42:26 +0200

    colorPalette done

---
+++ colorPalette/app.js
@@ -0,0 +1,77 @@
+const generateBtn = document.getElementById('generate-btn')
+const paletteContainer = document.querySelector('.palette-container')
+
+// addEventListeners
+
+generateBtn.addEventListener('click', generatePalette)
+
+paletteContainer.addEventListener('click', (e) => {
+
+    if(e.target.classList.contains('copy-btn')){
+        const hexValue = e.target.previousElementSibling.textContent
+
+        navigator.clipboard.writeText(hexValue)
+        .then(() => showSucces(e.target))
+        .catch((err) => console.log(err))
+    }
+    else if(e.target.classList.contains('color')){
+        const hexValue = e.target.nextElementSibling.querySelector('.hex-value').textContent
+        
+        navigator.clipboard.writeText(hexValue)
+        .then(() => showSucces(e.target.nextElementSibling.querySelector('.copy-btn')))
+        .catch((err) => console.log(err))
+    }
+})
+
+// Functions
+
+function showSucces(ele){
+    ele.classList.remove('far','fa-copy')
+    ele.classList.add('fas','fa-check')
+
+    ele.style.color = '#48bb78'
+
+    setTimeout(() => {
+        ele.classList.remove('fas','fa-check')
+        ele.classList.add('far','fa-copy')
+    
+        ele.style.color = ''
+    }, 1500);
+}
+
+function generatePalette() {
+    const colors = []
+    for (let i = 0; i < 5; i++) {
+        colors.push(generateRandomColor())
+    }
+    console.log(colors);
+
+    updatePaletteDisplay(colors)
+}
+
+function generateRandomColor() {
+    const letters = '0123456789ABCDEF'
+    let color = '#'
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+        
+    }
+
+    return color
+}
+
+function updatePaletteDisplay(colors){
+    const colorBoxes = document.querySelectorAll('.color-box')
+
+    colorBoxes.forEach((box,idx) => {
+        const color = colors[idx]
+        const colorBox = box.querySelector('.color')
+        const hexValue = box.querySelector('.hex-value')
+
+        colorBox.style.backgroundColor = color
+        hexValue.textContent = color
+    })
+}
+
+generatePalette()
